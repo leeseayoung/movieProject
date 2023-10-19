251d4eee5493d07a921262cf92b558e115dc1ff4
tnfusrltk12 <tnfusrltk12@naver.com>
2023-10-19 18:47:21 +0900

    [이세영]
    - hello world로 코드 실행

--- src/main/java/com/penguin/movie/ex/hello/Hello.java
+++ src/main/java/com/penguin/movie/ex/hello/Hello.java
@@ -1,13 +1,16 @@
 package com.penguin.movie.ex.hello;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 
 @Controller
 public class Hello {
-	 @ResponseBody
-	    @RequestMapping("/hello")
+	 	@ResponseBody
+	    @RequestMapping("/hello/test")
 	    public String helloWorld() {
 	        return "Hello World!!";
 	    }
